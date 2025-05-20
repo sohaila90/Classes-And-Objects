8c240cca6321b815d593e3c5cbb9f3a03ed3711d
sohaila90 <sohaila.al-khayat@hotmail.com>
2025-05-20 12:33:12 +0200

    list

--- Classes And Objects/Program.cs
+++ Classes And Objects/Program.cs
@@ -5,16 +5,20 @@ public abstract class Program
     public static void Main()
     {
         // stundent1 og 2 er 2 instanser som betyr 2 objekter
-        Student student1 = new Student (  "Sohaila", 34, "programmering",30 );
-        Student student2 = new Student ( "Johan", 20, "apputvikler", 26 );
+        List<Fag> hallo = null;
+        Student student1 = new Student (  "Sohaila", 34, hallo,30 );
+        Student student2 = new Student ( "Johan", 20, hallo, 26 );
         student1.PrintInfo();
         student2.PrintInfo();
         
+    
         Fag fag1 = new Fag ( 30, "programmering", 30 );
-        // Fag subject2 = new Fag {FageKode = 20, FagNavn = "apputvikler", AntallStudiePoeng = 20};
+        Fag subject2 = new Fag (20, "apputvikler", 20);
         fag1.PrintInfo();
-        // subject2.PrintInfo();
-        //
+        subject2.PrintInfo();
+        hallo = new List<Fag>();
+        hallo.Add(fag1);
+        
         Karakter karakter1 = new Karakter (student1, fag1, 3);
         Karakter karakater2 = new Karakter (student2, fag1,4);
         karakter1.PrintInfo();
@@ -22,6 +26,8 @@ public abstract class Program
         
         student1.AddKarakter(karakter1);
         Console.WriteLine($"Her er en karakter: "+student1.Karakterer[0].KarakterVerdi);
+        student1.AddFag(fag1);
+        Console.Write($"Her kommer fag: "+student1.Fags[0].FagNavn);
     }
 }
 
--- Classes And Objects/Student.cs
+++ Classes And Objects/Student.cs
@@ -7,29 +7,34 @@ public class Student
     public string Navn { get; private set; }
     public int Alder { get; private set; }
     public string StudeProgram { get; private set; }
-    public int StudentID { get; private set; }
+    public int StudentId { get; private set; }
 
     // lager liste for å gi karakterer
     public List<Karakter> Karakterer { get; private set; } = new List<Karakter>();
-    
+    public List<Fag> Fags { get; private set; } = new List<Fag>();
 
-    public Student(string navn, int alder, string studeProgram, int studentID)
+    public Student(string navn, int alder, List<Fag> fag, int studentId)
     {
         // dette er en constructor
         Navn = navn;
         Alder = alder;
-        StudeProgram = studeProgram;
-        StudentID = studentID;
+        Fags = fag;
+        StudentId = studentId;
     }
     
     public void PrintInfo()
     {
-       Console.WriteLine($"Elev: {Navn} Alder: {Alder} Studieprogram: {StudeProgram} Id: {StudentID}");
+       Console.WriteLine($"Elev: {Navn} Alder: {Alder} Studieprogram: {StudeProgram} Id: {StudentId}");
     }
 
     public void AddKarakter(Karakter karakter)
     {
         Karakterer.Add(karakter);
+    }
+
+    public void AddFag(Fag fags)
+    {
+        Fags.Add(fags);
     }
    
 }
