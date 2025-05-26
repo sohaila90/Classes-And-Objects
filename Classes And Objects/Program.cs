0bc0f8d520fa58cefce3d21f960e0cfcade73199
sohaila90 <sohaila.al-khayat@hotmail.com>
2025-05-26 10:38:59 +0200

    studiepoeng

--- Classes And Objects/Program.cs
+++ Classes And Objects/Program.cs
@@ -1,38 +1,74 @@
-﻿using Classes_And_Objects;
-// Hvorfor vil den endre fra using til namespace?
+﻿
+// namespace betyr at den bor her, men using bruker elementet, men ikke er her
+namespace Classes_And_Objects;
+
 public abstract class Program
 {
     public static void Main()
     {
-        // stundent1 og 2 er 2 instanser som betyr 2 objekter
-        List<Fag> hallo = null;
-        Student student1 = new Student (  "Sohaila", 34, hallo,30 );
-        Student student2 = new Student ( "Johan", 20, hallo, 26 );
-        student1.PrintInfo();
-        student2.PrintInfo();
-        
-    
+        Console.WriteLine("Studentadministrasjonssystem");
         Fag fag1 = new Fag ( 30, "programmering", 30 );
+        Fag fag2 = new Fag ( 20, "datamaskin", 20 );
+        Fag fag3 = new Fag ( 10, "naturfag", 10 );
         Fag subject2 = new Fag (20, "apputvikler", 20);
-        fag1.PrintInfo();
-        subject2.PrintInfo();
-        hallo = new List<Fag>();
-        hallo.Add(fag1);
+
+        // stundent1 og 2 er 2 instanser som betyr 2 objekter
+        Student student1 = new Student (  "Sohaila", 34,30 );
+        Student student2 = new Student ( "Johan", 20, 26 );
+        
+        // Trenger jeg disse eller ikke?
+        // student1.AddFag(fag1);
+        // student1.AddFag(fag2);
+        // student1.AddFag(fag3);
+        // student1.AddFag(subject2);
+        
+        // student2.AddFag(fag1);
+        // student2.AddFag(fag2);
+        // student2.AddFag(fag3);
+        // student2.AddFag(subject2);
+        
+        
+        
+        // for hvert fag skriv ut fagnavn
+        // foreach = “gjør noe med hver enkelt”
+        // // Det er som å si: “klapp hver kylling på hodet”
+        // da kan du bruke foreach
+        foreach (var fag in student1.Fags)
+        {
+            Console.WriteLine(fag.FagNavn);
+        }
+        
+        foreach (var bleie in student2.Fags)
+        {
+            Console.WriteLine(bleie.FagNavn);
+            
+        }
         
         Karakter karakter1 = new Karakter (student1, fag1, 3);
         Karakter karakater2 = new Karakter (student2, fag1,4);
-        karakter1.PrintInfo();
-        karakater2.PrintInfo();
+        student1.AddKarakter(karakter1);
+        student2.AddKarakter(karakater2);
+        
         
         student1.AddKarakter(karakter1);
-        Console.WriteLine($"Her er en karakter: "+student1.Karakterer[0].KarakterVerdi);
+
         student1.AddFag(fag1);
-        Console.Write($"Her kommer fag: "+student1.Fags[0].FagNavn);
+        student1.AddFag(fag2);
+        student1.AddFag(fag3);
+        
+
+        student1.PrintInfo();
+        fag1.PrintInfo();
+        fag2.PrintInfo();
+        fag3.PrintInfo();
+        subject2.PrintInfo();
+        Console.WriteLine($"Her er en karakter: "+student1.Karakterer[0].KarakterVerdi);
+        Console.WriteLine($"Totalt studiepoeng: "+student1.Studiepoeng()); 
+        
+        student2.PrintInfo();
+        // Console.Write($"Her kommer fag: "+student1.Fags[0].FagNavn);
     }
 }
-
-
-
 
 
 // var student = new Student("Sohaila", 34, "programmering", 25);
--- Classes And Objects/Student.cs
+++ Classes And Objects/Student.cs
@@ -6,35 +6,66 @@ public class Student
     // set kan endre verdien navn
     public string Navn { get; private set; }
     public int Alder { get; private set; }
-    public string StudeProgram { get; private set; }
+    public string StudieProgram { get; private set; }
     public int StudentId { get; private set; }
 
     // lager liste for å gi karakterer
-    public List<Karakter> Karakterer { get; private set; } = new List<Karakter>();
-    public List<Fag> Fags { get; private set; } = new List<Fag>();
+    public List<Karakter> Karakterer { get; private set; } 
+    
+    // du får tilgang til fags og blir tilknytta til objektet
+    public List<Fag> Fags { get; private set; }
 
-    public Student(string navn, int alder, List<Fag> fag, int studentId)
+    public Student(string navn, int alder, int studentId)
     {
         // dette er en constructor
         Navn = navn;
         Alder = alder;
-        Fags = fag;
         StudentId = studentId;
+        Karakterer = new List<Karakter>();
+        Fags = new List<Fag>();
     }
     
     public void PrintInfo()
     {
-       Console.WriteLine($"Elev: {Navn} Alder: {Alder} Studieprogram: {StudeProgram} Id: {StudentId}");
+       Console.WriteLine($"Elev: {Navn} Alder: {Alder} Studieprogram: {StudieProgram} Id: {StudentId}");
+       Gjennomsnittskarakter();
     }
+    
 
     public void AddKarakter(Karakter karakter)
     {
         Karakterer.Add(karakter);
     }
+    
 
     public void AddFag(Fag fags)
     {
         Fags.Add(fags);
+        
+        
     }
-   
+
+    private void Gjennomsnittskarakter()
+    {
+        var sum = 0;
+        // count samme som length
+        for (int i = 0; i < Karakterer.Count; i++)
+        {
+         sum += Karakterer[i].KarakterVerdi;   
+         
+        }
+        var gjennomsnitt = sum / Karakterer.Count;
+        Console.WriteLine($"Gjennomsnitts karakteren er: " +gjennomsnitt);
+    }
+
+    public int Studiepoeng()
+    {
+        var sum = 0;
+        for (int i = 0; i < Fags.Count; i++)
+        {
+            sum += Fags[i].AntallStudiePoeng;
+        }
+
+        return sum;
+    }
 }
